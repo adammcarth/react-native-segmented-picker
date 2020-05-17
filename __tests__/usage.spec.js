c8ccd8dc3e446c25d2c2d99fad493d6bd86afcbd
Adam McArthur <adam@adammcarthur.net>
2020-05-17 16:39:15 +1000

    Fix eslint configuration issue introduced with detox

--- __tests__/usage.spec.js
+++
@@ -1,9 +0,0 @@
-describe('<SegmentedPicker />', () => {
-  beforeEach(async () => {
-    await device.reloadReactNative();
-  });
-
-  test('Scroll through a 1 column example and check emitted selections', async () => {
-    await expect(element(by.id('EXAMPLE_A_1_COL'))).toBeVisible();
-  });
-});
